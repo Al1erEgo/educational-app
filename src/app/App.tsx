0eaa5f01739012affb579d800ec6e1e14bd0ca48
Al1erEgo <al1erego@gmail.com>
2023-04-03 16:58:07 +0200

    style: add globalStyle, remove .css

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
 import { Header } from '../components/c11-Header/Header'
 import { Pages } from '../pages'
+import { GlobalStyle } from '../styles'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Pages />
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
-import './index.css'
 
 import App from './app/App'
 import { store } from './store'
