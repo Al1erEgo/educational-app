79913918483d0fb46845162514a06bce7c393cc7
Al1erEgo <al1erego@gmail.com>
2023-04-04 12:51:19 +0200

    fix: restore baseUrl from variable inside rootAPI

--- src/store/rootAPI.ts
+++ src/store/rootAPI.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const rootAPI = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://neko-back.herokuapp.com/2.0/', //import.meta.env.VITE_BASE_URL,
+    baseUrl: import.meta.env.VITE_BASE_URL,
     credentials: 'include',
   }),
   endpoints: () => ({}),
