c8c0156fd28d23ebf37d4a31cf136ff0101702c0
Al1erEgo <al1erego@gmail.com>
2023-04-05 12:14:03 +0200

    style: add StyledTitle to auth-style.ts

--- src/modules/auth/styles/auth-style.ts
+++ src/modules/auth/styles/auth-style.ts
@@ -8,11 +8,11 @@ export const AuthContainer = styled.div`
 `
 
 export const FormWrapper = styled.div`
-  //width: 100%;
-  //max-width: 26rem;
-  //margin: 0 auto;
-  //border-radius: 8px;
-  //box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 26rem;
+  margin: 0 auto;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `
 
 export const StyledTitle = styled.div`
