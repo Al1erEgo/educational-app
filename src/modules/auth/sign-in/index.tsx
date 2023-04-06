2d98c0378eb4d1f3ee9ab70dcd97b84b9e33cf9c
Al1erEgo <al1erego@gmail.com>
2023-04-06 11:48:13 +0200

    feat: login page, stage 2

--- src/modules/auth/sign-in/index.tsx
+++ src/modules/auth/sign-in/index.tsx
@@ -1,11 +1,16 @@
-import { Card, Checkbox, Form, Input } from 'antd'
+import { Button, Checkbox, Form, Input, Typography } from 'antd'
 
-import { StyledNavLink, StyledP } from '../styles'
+import { MAIN_PATH } from '../../../constants'
+import { AUTH_PATH } from '../constants'
+import { cardHeadStyle, StyledCard, StyledNavLink, StyledP } from '../styles'
+
+import { ForgotPasswordLink } from './styles'
+
+const { Text } = Typography
 
 export const SignIn = () => {
   return (
-    <Card>
-      {/*<StyledTitle>Sign In</StyledTitle>*/}
+    <StyledCard title={'Sign In'} headStyle={cardHeadStyle}>
       <Form>
         <Form.Item
           name="username"
@@ -16,7 +21,7 @@ export const SignIn = () => {
             },
           ]}
         >
-          <Input />
+          <Input placeholder="Email" autoComplete="email" />
         </Form.Item>
         <Form.Item
           name="password"
@@ -27,7 +32,7 @@ export const SignIn = () => {
             },
           ]}
         >
-          <Input.Password />
+          <Input.Password placeholder="Password" autoComplete="password" />
         </Form.Item>
         <Form.Item
           name="remember"
@@ -39,15 +44,25 @@ export const SignIn = () => {
         >
           <Checkbox>Remember me</Checkbox>
         </Form.Item>
+        <ForgotPasswordLink to={`${MAIN_PATH.Auth}${AUTH_PATH.ResetPassword}`}>
+          Forgot password?
+        </ForgotPasswordLink>
         <Form.Item>
-          {/*<StyledButton type="primary" htmlType="submit" loading={false}>*/}
-          {/*  Sign In*/}
-          {/*</StyledButton>*/}
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            loading={false}
+            style={{ fontWeight: '500' }}
+            block
+          >
+            Sign In
+          </Button>
         </Form.Item>
       </Form>
       <StyledP>Have no account?</StyledP>
 
-      <StyledNavLink to="/auth/sign-up">Sign Up</StyledNavLink>
-    </Card>
+      <StyledNavLink to={`${MAIN_PATH.Auth}${AUTH_PATH.SignUp}`}>Sign Up</StyledNavLink>
+    </StyledCard>
   )
 }
