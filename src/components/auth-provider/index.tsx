67f7056ab368c50c289f476b14e1b287105bafba
Al1erEgo <al1erego@gmail.com>
2023-04-09 16:22:25 +0200

    fix: authProvider now do not send authMe request

--- src/components/auth-provider/index.tsx
+++ src/components/auth-provider/index.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
 import { MAIN_PATH } from '../../constants'
-import { useAuthorised } from '../../modules/auth/hooks'
+import { authApi } from '../../modules/auth/auth-api'
 
 export const AuthProvider: React.FC = () => {
-  const { isSuccess } = useAuthorised()
+  const [, { isSuccess }] = authApi.useLazyAuthMeQuery()
 
   return isSuccess ? <Outlet /> : <Navigate to={`${MAIN_PATH.Auth}`} />
 }
