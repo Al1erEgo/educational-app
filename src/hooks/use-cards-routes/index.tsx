975361744b5630bc24fd82ae133b0a4a295069d2
Al1erEgo <al1erego@gmail.com>
2023-04-13 01:01:56 +0200

    docs: add comments in to useCardsRoutes

--- src/hooks/use-cards-routes/index.tsx
+++ src/hooks/use-cards-routes/index.tsx
@@ -21,17 +21,22 @@ type UseGuestRoutesType = (
   guestRoute: string
 ) => React.ReactElement | null
 
+//hook get routes object and two conditional routes for guest or loginned user
 export const useCardsRoutes: UseGuestRoutesType = (routes, userRoute, guestRoute) => {
   const { isAuthorised } = useAuthorised()
 
+  //which page will be default depends on auth status
   const defaultPage = isAuthorised ? <Navigate to={userRoute} /> : <Navigate to={guestRoute} />
 
+  //constructing routes array for useRouter hook from react-router-dom without guest routes
+  //private routes works in any case, but for guest will be redirected to authorisation
   let prepareRoutes: RouteType[] = [
     { ...routes.ROOT_ROUTE[0], element: defaultPage },
     { element: <AuthProvider />, children: routes.PRIVATE_ROUTES },
     ...routes.MAIN_ROUTES,
   ]
 
+  //add guest routes if user is not logged in
   if (!isAuthorised) {
     prepareRoutes = [...prepareRoutes, ...routes.GUEST_ROUTES]
   }
