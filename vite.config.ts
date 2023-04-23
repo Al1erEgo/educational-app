a639f77534fa75ce4bbf9f77dafb96830f1e19bf
Al1erEgo <al1erego@gmail.com>
2023-04-23 15:38:25 +0200

    fix: trying to configure Vite for successful deploying

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Al1erego/friday-project',
+  base: '/friday-project/',
   plugins: [react()],
   build: {
     rollupOptions: {
