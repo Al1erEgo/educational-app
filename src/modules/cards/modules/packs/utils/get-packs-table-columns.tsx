92d744bd875e8b7dddb38e09163876ae3cb6f629
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-09 19:33:38 +0300

    feat: add navigate to learn and simplify actions render

--- src/modules/cards/modules/packs/utils/get-packs-table-columns.tsx
+++ src/modules/cards/modules/packs/utils/get-packs-table-columns.tsx
@@ -4,7 +4,117 @@ import { DeleteOutlined, EditOutlined, InfoCircleTwoTone } from '@ant-design/ico
 import { Space, Tooltip } from 'antd'
 import { NavLink } from 'react-router-dom'
 
-import { LoginResponseType } from '../../../../auth/api/types'
+import { LoginResponseType } from '../../../../auth/types'
+import { useModalContext } from '../../../../modal-provider/hooks'
+import { MY_BUTTON_NAME } from '../../../constants'
+import { PacksModal, DeleteModal } from '../components/packs-modals'
+import { packsTableColumns } from '../constants'
+import { HandlerPacksFunctionType, PacksTableDataColumnsType, PackType } from '../types'
+
+type GetPacksTableColumnsType = (
+  activeButton: string,
+  userData: LoginResponseType | undefined,
+  handleEdit: HandlerPacksFunctionType,
+  handleDelete: HandlerPacksFunctionType,
+  handleOk: (id?: string, newName?: string) => void,
+  handleDeleteOk: (id?: string) => void
+) => PacksTableDataColumnsType[]
+
+export const getPacksTableColumns: GetPacksTableColumnsType = (
+  activeButton,
+  userData,
+  handleEdit,
+  handleDelete,
+  handleOk,
+  handleDeleteOk
+) => {
+  const { showModal } = useModalContext()
+
+  return [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      sorter: true,
+      render: (text: string, pack: PackType) => (
+        <NavLink
+          to={`/cards/packs/${pack._id}?name=${pack.name}&own=${pack?.user_id === userData?._id}`}
+        >
+          {text}
+        </NavLink>
+      ),
+    },
+    ...packsTableColumns,
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      render: (text: string, pack: PackType) => {
+        const hasCards = pack.cardsCount ? pack.cardsCount > 0 : false
+
+        const learnAction = hasCards ? (
+          <NavLink to={`/learn/${pack._id}`}>
+            <InfoCircleTwoTone />
+          </NavLink>
+        ) : (
+          <InfoCircleTwoTone
+            twoToneColor="lightgrey"
+            onClick={() => {
+              console.log(pack)
+            }}
+          />
+        )
+
+        const learnTooltipTitle = hasCards ? 'Learn' : 'No cards to learn'
+
+        const handleEditClick = () => {
+          showModal({
+            title: 'Edit Pack',
+            content: (
+              <PacksModal
+                key={pack._id}
+                editing
+                onOk={handleOk}
+                packName={pack.name}
+                id={pack._id}
+                isPrivate={pack.isPrivate}
+              />
+            ),
+          })
+        }
+
+        const handleDeleteClick = () => {
+          showModal({
+            title: 'Delete Pack',
+            content: <DeleteModal onOk={() => handleDeleteOk(pack._id)} packName={pack.name} />,
+          })
+        }
+
+        return activeButton === MY_BUTTON_NAME || pack?.user_id === userData?._id ? (
+          <Space size="middle">
+            <Tooltip title={learnTooltipTitle}>{learnAction}</Tooltip>
+
+            <Tooltip title="Edit">
+              <EditOutlined onClick={handleEditClick} />
+            </Tooltip>
+
+            <Tooltip title="Delete">
+              <DeleteOutlined onClick={handleDeleteClick} />
+            </Tooltip>
+          </Space>
+        ) : (
+          <Tooltip title={learnTooltipTitle}>{learnAction}</Tooltip>
+        )
+      },
+    },
+  ]
+}
+
+/*import React from 'react'
+
+import { DeleteOutlined, EditOutlined, InfoCircleTwoTone } from '@ant-design/icons'
+import { Space, Tooltip } from 'antd'
+import { NavLink } from 'react-router-dom'
+
+import { LoginResponseType } from '../../../../auth/types'
 import { useModalContext } from '../../../../modal-provider/hooks'
 import { MY_BUTTON_NAME } from '../../../constants'
 import { PacksModal, DeleteModal } from '../components/packs-modals'
@@ -105,4 +215,4 @@ export const getPacksTableColumns: GetPacksTableColumnsType = (
       },
     },
   ]
-}
+}*/
