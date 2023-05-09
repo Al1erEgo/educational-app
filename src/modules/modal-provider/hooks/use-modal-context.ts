968e01ec975c187f263c338b6273d8a323c50ded
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-09 22:36:57 +0300

    feat: add jsdoc

--- src/modules/modal-provider/hooks/use-modal-context.ts
+++ src/modules/modal-provider/hooks/use-modal-context.ts
@@ -2,6 +2,10 @@ import { createContext, useContext } from 'react'
 
 import { ModalContextType } from '../types/types'
 
+/**
+ * A context object for managing modal state and providing it to child components.
+ * @type {React.Context<ModalContextType>}
+ */
 export const ModalContext = createContext<ModalContextType>({
   modalConfig: {
     title: '',
@@ -11,4 +15,8 @@ export const ModalContext = createContext<ModalContextType>({
   hideModal: () => {},
 })
 
+/**
+ * A custom React hook that returns the current modal context.
+ * @returns {ModalContextType} An object containing the current modal configuration, as well as functions to show and hide the modal.
+ */
 export const useModalContext = () => useContext(ModalContext)
