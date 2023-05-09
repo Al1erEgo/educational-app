220e18ad57258f4eab9a9a2b84262119c10517f9
Al1erEgo <al1erego@gmail.com>
2023-05-09 12:23:23 +0200

    fix: refactor  wiseSortingCards function

--- src/modules/cards/modules/learn/utils/wise-sorting-cards.ts
+++ src/modules/cards/modules/learn/utils/wise-sorting-cards.ts
@@ -3,13 +3,5 @@ import { CardType } from '../../../api'
 type WiseSortingCardsType = (cards: CardType[]) => CardType[]
 
 export const wiseSortingCards: WiseSortingCardsType = cards => {
-  const sortedCards = cards
-    .map(card => ({ ...card, priority: card.shots / card.grade }))
-    .sort((cardA, cardB) => cardA.priority - cardB.priority)
-
-  sortedCards.forEach(card => {
-    delete card.priority
-  })
-
-  return sortedCards
+  return [...cards].sort((cardA, cardB) => cardA.shots / cardA.grade - cardB.shots / cardB.grade)
 }
