66f4467ccb301e751e2b21858cdddcc4b236ef23
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-23 19:34:01 +0300

    fix: cards modal type

--- src/modules/cards/types/cards-modals.ts
+++ src/modules/cards/types/cards-modals.ts
@@ -1,5 +1,11 @@
+import {
+  DeleteCardRequestType,
+  NewCardRequestType,
+  UpdateCardRequestType,
+} from '../api'
+
 import { PackModalCardPayloadType } from './pack-modals'
-import { PacksModalPayloadType } from './packs-modals'
+import { PacksModalPayloadType, PacksModalsHandlersType } from './packs-modals'
 
 export type CardsModalsOnSubmitType<T> = (payload: T) => void
 
@@ -14,3 +20,15 @@ export type CardsModalsHandlerType<T> = CardsModalsOnSubmitType<T>
 export type CardsModalPayloadType =
   | PackModalCardPayloadType
   | PacksModalPayloadType
+
+export type CardsModalsHandlersType = {
+  addCardModal: CardsModalsHandlerType<NewCardRequestType>
+  updateCardModal: CardsModalsHandlerType<UpdateCardRequestType>
+  deleteCardModal: CardsModalsHandlerType<DeleteCardRequestType>
+} & PacksModalsHandlersType
+
+export enum ModalCardsFormat {
+  TEXT = 'text',
+  IMG = 'img',
+  IMGPACK = 'imgPack',
+}
--- src/modules/cards/types/pack-modals.ts
+++ src/modules/cards/types/pack-modals.ts
@@ -1,31 +1,12 @@
-import {
-  DeleteCardRequestType,
-  NewCardRequestType,
-  UpdateCardRequestType,
-} from '../api'
-
-import { CardsModalsHandlerType } from './cards-modals'
-import { PacksModalsHandlersType } from './packs-modals'
+import { NewCardRequestType, UpdateCardRequestType } from '../api'
 
 export type PackModalCardPayloadType =
   | NewCardRequestType
   | UpdateCardRequestType
-
-export type CardsModalsHandlersType = {
-  addCardModal: CardsModalsHandlerType<NewCardRequestType>
-  updateCardModal: CardsModalsHandlerType<UpdateCardRequestType>
-  deleteCardModal: CardsModalsHandlerType<DeleteCardRequestType>
-} & PacksModalsHandlersType
 
 export type ModalCardFormDataType = {
   question?: string
   answer?: string
   questionImg?: string
   answerImg?: string
-}
-
-export enum ModalCardsFormat {
-  TEXT = 'text',
-  IMG = 'img',
-  IMGPACK = 'imgPack',
 }
