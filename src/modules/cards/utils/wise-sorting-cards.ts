aa9120fec44ac3528c4a4198ec0770da32d707ac
Al1erEgo <al1erego@gmail.com>
2023-05-25 13:27:43 +0200

    fix: wise-sorting-cards.ts reverse sorting

--- src/modules/cards/utils/wise-sorting-cards.ts
+++ src/modules/cards/utils/wise-sorting-cards.ts
@@ -4,6 +4,6 @@ type WiseSortingCardsType = (cards: CardType[]) => CardType[]
 
 export const wiseSortingCards: WiseSortingCardsType = cards => {
   return [...cards].sort(
-    (cardA, cardB) => cardA.shots / cardA.grade - cardB.shots / cardB.grade
+    (cardA, cardB) => cardB.shots / cardB.grade - cardA.shots / cardA.grade
   )
 }
