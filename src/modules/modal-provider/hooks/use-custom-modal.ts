62de2de3152a2d629a114715ca1a554ecc59f946
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-26 13:31:24 +0300

    fix: modal-providers hooks

--- src/modules/modal-provider/hooks/use-custom-modal.ts
+++ src/modules/modal-provider/hooks/use-custom-modal.ts
@@ -1,19 +1,3 @@
-/**
- * Represents the configuration for a modal.
- * @typedef {Object} ModalConfig
- * @property {string} title - The title of the modal.
- * @property {React.ReactNode} content - The content to be displayed in the modal.
- */
-
-/**
- * The type of the modal context object returned by the custom hook.
- * @typedef {Object} ModalContextType
- * @property {ModalConfig} modalConfig - The current modal configuration.
- * @property {function} showModal - A function that displays a modal with the specified configuration.
- * @property {function} hideModal - A function that hides the currently displayed modal.
- * @property {Object} showModal - The configuration for the modal to be displayed.
- */
-
 import { useState } from 'react'
 
 import {
@@ -29,25 +13,13 @@ import {
  as well as functions to show and hide the modal.
  */
 export const useCustomModal: UseCustomModalType = () => {
-  /**
-   * The current modal configuration.
-   * @type {ModalConfig}
-   */
-
   const [modalConfig, setModalConfig] = useState<ModalConfig>({
     title: '',
     content: null,
   })
-  /**
-   * Shows a modal with the specified configuration.
-   * @param {ModalConfig} config - The configuration for the modal to be displayed.
-   */
   const showModal = (config: ModalConfig) => {
     setModalConfig(config)
   }
-  /**
-   * Hides the currently displayed modal.
-   */
   const hideModal = () => {
     setModalConfig({
       title: '',
