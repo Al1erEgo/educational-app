f7771bd4dcdb46bbb10357730e209ad3009d2023
Aleksey Kriuchkov <aleksejkriuchkov@gmail.com>
2023-05-30 15:18:55 +0300

    feat: clicking on the logo redirect in Cards

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,18 @@
 import { FC } from 'react'
 
-import { StyledHeader, StyledLogo, StyledProfileWidgetWrapper} from './styles'
+import { StyledHeader, StyledLogo, StyledProfileWidgetWrapper } from './styles'
 
 import logo from '@/assets/incubator-logo.png'
+import { useNavigateHandler } from '@/hooks'
 import { ProfileWidget } from '@/modules/auth/components'
+import { ABSOLUTE_CARD_PATH } from '@/modules/cards/constants'
 
 export const Header: FC = () => {
+  const handleGoToCardsRedirect = useNavigateHandler(ABSOLUTE_CARD_PATH.Packs)
+
   return (
     <StyledHeader>
-      <StyledLogo src={logo} />
+      <StyledLogo src={logo} onClick={handleGoToCardsRedirect} />
       <StyledProfileWidgetWrapper>
         <ProfileWidget />
       </StyledProfileWidgetWrapper>
