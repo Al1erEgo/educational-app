7f0a3328457ceb3e9936a74dbe91002259cc81a2
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-05-31 19:26:20 +0300

    feat: add slider to use-packs-search-params

--- src/modules/cards/hooks/use-packs-handlers.ts
+++ src/modules/cards/hooks/use-packs-handlers.ts
@@ -17,6 +17,7 @@ import {
 type SearchParamsType = {
   setSearchValue: (searchValue: string) => void
   setPagination: (pagination: TablePaginationConfig) => void
+  setSliderChanged: (sliderValues: [number, number]) => void
 }
 
 type UsePacksHandlersType = (
@@ -67,6 +68,7 @@ export const usePacksHandlers: UsePacksHandlersType = (
         maxCardsCount: value[1],
       }))
     }
+    searchParams.setSliderChanged(value as [number, number])
   }, [])
 
   const handleClearFilters: HandleClearFiltersType = () => {
@@ -85,6 +87,7 @@ export const usePacksHandlers: UsePacksHandlersType = (
     }))
     searchParams.setSearchValue('')
     searchParams.setPagination({})
+    searchParams.setSliderChanged([0, 0])
   }
   const handleToggleButton: HandleToggleButtonType = buttonName => {
     setTableParams(prevState => ({
@@ -103,146 +106,3 @@ export const usePacksHandlers: UsePacksHandlersType = (
     modalHandlers,
   }
 }
-
-/*import React, { useCallback, useEffect } from 'react'
-
- import { useNavigate, useSearchParams } from 'react-router-dom'
-
- import { useCardsModals } from '@/modules/cards/hooks/use-cards-modals'
- import {
- CardsModalsHandlersType,
- CardsMutationsObjType,
- HandleClearFiltersType,
- HandlePacksSearchType,
- HandlePacksTableChangeType,
- HandleSliderChangeType,
- HandleToggleButtonType,
- PacksTableParamsType,
- } from '@/modules/cards/types'
-
- type UsePacksHandlersType = (
- setTableParams: React.Dispatch<React.SetStateAction<PacksTableParamsType>>,
- tableParams: any,
- mutations: CardsMutationsObjType
- ) => {
- handleTableChange: HandlePacksTableChangeType
- handlePacksSearch: HandlePacksSearchType
- handleSliderChange: HandleSliderChangeType
- handleToggleButton: HandleToggleButtonType
- handleClearFilters: HandleClearFiltersType
- modalHandlers: CardsModalsHandlersType
- }
-
- export const usePacksHandlers: UsePacksHandlersType = (
- setTableParams,
- tableParams,
- mutations
- ) => {
- const modalHandlers = useCardsModals(mutations)
-
- const [searchParams, setSearchParams] = useSearchParams({
- search: '',
- current: '',
- pageSize: '',
- })
- const navigate = useNavigate()
-
- // Function to retrieve the search parameter from the URL and set it in the component state
- const initializeSearchParams = () => {
- const searchValue = searchParams.get('search') || ''
-
- setTableParams(prevState => ({ ...prevState, searchValue }))
- }
-
- // Call the initializeSearchParams function when the component mounts
- useEffect(() => {
- initializeSearchParams()
- }, [])
-
- const handlePacksSearch: HandlePacksSearchType = searchValue => {
- setTableParams(prevState => ({ ...prevState, searchValue }))
- setSearchParams(prevSearchParams => ({
- ...prevSearchParams,
- search: searchValue,
- }))
- localStorage.setItem('searchValue', searchValue)
- const params = new URLSearchParams(searchParams.toString())
-
- params.set('search', searchValue)
-
- navigate(`/cards/packs?${params}`)
- }
-
- const handleTableChange: HandlePacksTableChangeType = (
- pagination,
- filters,
- sorter
- ) => {
- setTableParams(prevState => ({
- ...prevState,
- pagination,
- ...sorter,
- }))
- setSearchParams(prevSearchParams => ({
- ...prevSearchParams,
- pagination: JSON.stringify({
- current: pagination.current,
- pageSize: pagination.pageSize,
- }),
- }))
- localStorage.setItem('pagination', JSON.stringify(pagination))
- const params = new URLSearchParams({
- search: tableParams.searchValue,
- current: String(pagination.current),
- pageSize: String(pagination.pageSize),
- })
-
- navigate(`/cards/packs?${params.toString()}`)
- }
-
- const handleSliderChange: HandleSliderChangeType = useCallback(value => {
- if (Array.isArray(value)) {
- setTableParams(prevState => ({
- ...prevState,
- minCardsCount: value[0],
- maxCardsCount: value[1],
- }))
- }
- }, [])
-
- const handleClearFilters: HandleClearFiltersType = () => {
- setTableParams(prevState => ({
- ...prevState,
- pagination: {
- current: 1,
- pageSize: 10,
- },
- field: '',
- order: null,
- sortPacks: '',
- searchValue: '',
- minCardsCount: undefined,
- maxCardsCount: undefined,
- }))
-
- setSearchParams({})
- localStorage.removeItem('searchValue')
- localStorage.removeItem('pagination')
- }
- const handleToggleButton: HandleToggleButtonType = buttonName => {
- setTableParams(prevState => ({
- ...prevState,
- activeButton: buttonName,
- }))
- handleClearFilters()
- }
-
- return {
- handleTableChange,
- handlePacksSearch,
- handleSliderChange,
- handleToggleButton,
- handleClearFilters,
- modalHandlers,
- }
- }*/
--- src/modules/cards/hooks/use-packs-search-params.ts
+++ src/modules/cards/hooks/use-packs-search-params.ts
@@ -20,9 +20,68 @@ export const usePacksSearchParams = () => {
     setSearchParams(params)
   }
 
-  return {
-    searchParams,
+  const setSliderChanged = (sliderValues: [number, number]) => {
+    const params = new URLSearchParams(searchParams.toString())
+
+    params.set('minCardsCount', String(sliderValues[0]))
+    params.set('maxCardsCount', String(sliderValues[1]))
+
+    setSearchParams(params)
+  }
+
+  const searchActions = {
     setSearchValue,
     setPagination,
+    setSliderChanged,
+  }
+
+  return {
+    searchParams,
+    ...searchActions,
   }
 }
+
+/*import { TablePaginationConfig } from 'antd/es/table/InternalTable'
+ import { useSearchParams } from 'react-router-dom'
+
+ export const usePacksSearchParams = () => {
+ const [searchParams, setSearchParams] = useSearchParams({})
+
+ const setSearchValue = (searchValue: string) => {
+ const params = new URLSearchParams(searchParams.toString())
+
+ params.set('search', searchValue)
+
+ setSearchParams(params)
+ }
+
+ const setPagination = (pagination: TablePaginationConfig) => {
+ const params = new URLSearchParams(searchParams.toString())
+
+ params.set('pagination', JSON.stringify(pagination))
+
+ setSearchParams(params)
+ }
+
+ const setSliderChanged = (sliderValues: [number, number]) => {
+ const params = new URLSearchParams(searchParams.toString())
+
+ params.set('minCardsCount', String(sliderValues[0]))
+ params.set('maxCardsCount', String(sliderValues[1]))
+
+ setSearchParams(params)
+ localStorage.setItem('sliderValues', JSON.stringify(sliderValues))
+ }
+
+ const searchActions = {
+ setSearchValue,
+ setPagination,
+ setSliderChanged,
+ }
+
+ return {
+ searchParams,
+ ...searchActions,
+ }
+ }
+ */
