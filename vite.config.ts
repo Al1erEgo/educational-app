faadd196c6692552633e22baa0c5a6900f4ffb0e
Al1erEgo <al1erego@gmail.com>
2023-06-13 14:07:23 +0200

    fix: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/educational-app/',
   //base: '/educational-app/',
   plugins: [react()],
   resolve: {
