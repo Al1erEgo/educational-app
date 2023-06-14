56d05b8c2dda766bf3ce1f9d1cec52c3799091de
Al1erEgo <al1erego@gmail.com>
2023-06-14 18:45:11 +0200

    fix: part of TODOs st6

--- src/modules/cards/index.tsx
+++ src/modules/cards/index.tsx
@@ -13,14 +13,10 @@ export const Cards = () => {
 
   return (
     <Routes>
-      <Route
-        path={CARD_PATH.Root}
-        element={<Navigate to={`${path.pathname}${CARD_PATH.Packs}`} />}
-      />
+      <Route path={CARD_PATH.Root} element={<Navigate to={`${path.pathname}${CARD_PATH.Packs}`} />} />
       <Route path={`${CARD_PATH.Learn}/:packId`} element={<Learn />} />
       <Route path={CARD_PATH.Packs} element={<Packs />} />
       <Route path={CARD_PATH.Pack} element={<Pack />} />
-      {/* TODO check that global error router handler can work for all errors. If yes, remove modules Error404 usages*/}
       <Route path={CARD_PATH.Error} element={<Error404 />} />
     </Routes>
   )
