d5c32ac95fd6f2fb87f014e7b7218d3b0a5c968b
Al1erEgo <al1erego@gmail.com>
2023-06-14 19:08:07 +0200

    fix: part of TODOs st8

--- src/modules/auth/pages/new-password/index.tsx
+++ src/modules/auth/pages/new-password/index.tsx
@@ -4,24 +4,16 @@ import { Form } from 'antd'
 import { useParams } from 'react-router-dom'
 
 import { ErrorMessageHandler } from '@/components'
-import {
-  ConfirmationMessage,
-  FormButton,
-  FormInput,
-} from '@/modules/auth/components'
+import { ConfirmationMessage, FormButton, FormInput } from '@/modules/auth/components'
 import { ABSOLUTE_AUTH_PATH } from '@/modules/auth/constants'
 import { useFormData } from '@/modules/auth/hooks'
 import { StyledCard, StyledNavLink, StyledText } from '@/modules/auth/styles'
 import { NewPasswordFormInputs } from '@/modules/auth/types'
 
-// TODO rename all pages components: add suffix PAGE
 export const NewPassword: FC = () => {
   const { token } = useParams()
-  const [
-    onSubmit,
-    { handleSubmit, control, setError, errors },
-    { isLoading, error, isSuccess },
-  ] = useFormData<NewPasswordFormInputs>('newPassword')
+  const [onSubmit, { handleSubmit, control, setError, errors }, { isLoading, error, isSuccess }] =
+    useFormData<NewPasswordFormInputs>('newPassword')
 
   const handleNewPasswordSubmit = async (data: NewPasswordFormInputs) => {
     if (!token) {
@@ -41,15 +33,11 @@ export const NewPassword: FC = () => {
       <Form onFinish={handleSubmit(handleNewPasswordSubmit)}>
         {/* TODO {...formInputsConfigs['password']} */}
         <FormInput name="password" control={control} error={errors.password} />
-        <StyledText type="secondary">
-          Create new password and we will send you further instructions to email
-        </StyledText>
+        <StyledText type="secondary">Create new password and we will send you further instructions to email</StyledText>
         <ErrorMessageHandler serverError={error} />
         <FormButton loading={isLoading}>Create new Password</FormButton>
       </Form>
-      <StyledNavLink to={ABSOLUTE_AUTH_PATH.ResetPassword}>
-        Back to Send Email form
-      </StyledNavLink>
+      <StyledNavLink to={ABSOLUTE_AUTH_PATH.ResetPassword}>Back to Send Email form</StyledNavLink>
     </StyledCard>
   )
 }
